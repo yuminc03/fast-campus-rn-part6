04358b700580d32bbdff9655b5069cd7992925d5
Yumin Chu <yuminc03@gmail.com>
2024-05-28 11:05:34 +0900

    Feat: 로그인 상태에서는 Home, 로그아웃 상태에서는 로그인,회원가입 화면 띄우기, Loading 화면 보여주기

--- ChatApp/App.tsx
+++ ChatApp/App.tsx
@@ -1,20 +1,43 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
 
 import { RootStackParamList } from './src/types';
 import SignupScreen from './src/SignupScreen/SignupScreen';
 import AuthProvider from './src/components/AuthProvider';
 import SigninScreen from './src/SigninScreen/SigninScreen';
+import AuthContext from './src/components/AuthContext';
+import HomeScreen from './src/HomeScreen/HomeScreen';
+import LoadingScreen from './src/LoadingScreen/LoadingScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Screens = () => {
+  const { user, processingSignin, processingSignup, initialized } =
+    useContext(AuthContext);
+  const renderRootStack = useCallback(() => {
+    if (!initialized) {
+      return <Stack.Screen name="Loading" component={LoadingScreen} />;
+    }
+
+    if (user != null && !processingSignup && !processingSignin) {
+      // login
+      return <Stack.Screen name="Home" component={HomeScreen} />;
+    }
+
+    // logout
+    return (
+      <>
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Signin" component={SigninScreen} />
+      </>
+    );
+  }, [initialized, processingSignin, processingSignup, user]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Signin" component={SigninScreen} />
+        {renderRootStack()}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ ChatApp/src/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Screen from '../components/Screen';
+
+const HomeScreen = () => {
+  return <Screen title="홈" />;
+};
+
+export default HomeScreen;
