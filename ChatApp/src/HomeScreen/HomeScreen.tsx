342d826dcdca69fd68a6f9e0dcf73e2588adc0a2
Yumin Chu <yuminc03@gmail.com>
2024-05-28 13:15:42 +0900

    Feat: 다른 사용자를 보여주는 List UI

--- ChatApp/src/HomeScreen/HomeScreen.tsx
+++ ChatApp/src/HomeScreen/HomeScreen.tsx
@@ -1,17 +1,55 @@
-import React, { useCallback, useContext } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 import Screen from '../components/Screen';
 import AuthContext from '../components/AuthContext';
 import { Colors } from '../modules/Colors';
+import { Collections, User } from '../types';
 
 const HomeScreen = () => {
   const { user: me } = useContext(AuthContext);
+  const [loadingUsers, setLoadingUsers] = useState(false);
+  const [users, setUsers] = useState<User[]>([]);
 
   const onPressLogout = useCallback(() => {
     auth().signOut();
   }, []);
+
+  const loadUsers = useCallback(async () => {
+    try {
+      setLoadingUsers(true);
+      const snapshot = await firestore().collection(Collections.USERS).get();
+      setUsers(
+        snapshot.docs
+          .map(doc => doc.data() as User)
+          .filter(u => u.userId !== me?.userId),
+      );
+    } finally {
+      setLoadingUsers(false);
+    }
+  }, [me?.userId]);
+
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]);
+
+  const renderLoading = useCallback(
+    () => (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator />
+      </View>
+    ),
+    [],
+  );
 
   if (me == null) {
     return null;
@@ -31,6 +69,35 @@ const HomeScreen = () => {
               <Text style={styles.logoutText}>로그아웃</Text>
             </TouchableOpacity>
           </View>
+        </View>
+        <View style={styles.userListSection}>
+          {loadingUsers ? (
+            renderLoading()
+          ) : (
+            <>
+              <Text style={styles.sectionTitleText}>
+                다른 사용자와 대화해보세요!
+              </Text>
+              <FlatList
+                style={styles.userList}
+                data={users}
+                renderItem={({ item: user }) => (
+                  <TouchableOpacity
+                    style={styles.userListItem}
+                    onPress={() => {}}>
+                    <Text style={styles.otherNameText}>{user.name}</Text>
+                    <Text style={styles.otherEmailText}>{user.email}</Text>
+                  </TouchableOpacity>
+                )}
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
+                ListEmptyComponent={() => {
+                  return (
+                    <Text style={styles.emptyText}>사용자가 없습니다.</Text>
+                  );
+                }}
+              />
+            </>
+          )}
         </View>
       </View>
     </Screen>
@@ -71,6 +138,39 @@ const styles = StyleSheet.create({
   logoutText: {
     color: Colors.WHITE,
     fontSize: 14,
+  },
+  userListSection: {
+    marginTop: 40,
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userList: {
+    flex: 1,
+  },
+  userListItem: {
+    backgroundColor: Colors.LIGHT_GRAY,
+    borderRadius: 12,
+    padding: 20,
+  },
+  otherNameText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.BLACK,
+  },
+  otherEmailText: {
+    marginTop: 4,
+    fontSize: 14,
+    color: Colors.BLACK,
+  },
+  separator: {
+    height: 10,
+  },
+  emptyText: {
+    color: Colors.BLACK,
   },
 });
 
--- ChatApp/src/modules/Colors.ts
+++ ChatApp/src/modules/Colors.ts
@@ -1,6 +1,7 @@
 export const Colors = {
   BLACK: 'black',
   GRAY: 'gray',
+  LIGHT_GRAY: '#d3d9df',
   RED: 'red',
   WHITE: 'white',
 };
