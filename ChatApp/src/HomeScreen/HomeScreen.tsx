9b19752799d114a08a312a165a856f381aaa85dd
Yumin Chu <yuminc03@gmail.com>
2024-05-28 11:20:44 +0900

    Feat: 홈 화면 나의 정보, 로그아웃 버튼 UI

--- ChatApp/src/HomeScreen/HomeScreen.tsx
+++ ChatApp/src/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
 import Screen from '../components/Screen';
+import AuthContext from '../components/AuthContext';
+import { Colors } from '../modules/Colors';
 
 const HomeScreen = () => {
-  return <Screen title="홈" />;
+  const { user: me } = useContext(AuthContext);
+
+  const onPressLogout = useCallback(() => {
+    auth().signOut();
+  }, []);
+
+  if (me == null) {
+    return null;
+  }
+
+  return (
+    <Screen title="홈">
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.sectionTitleText}>나의 정보</Text>
+          <View style={styles.userSectionContent}>
+            <View style={styles.myProfile}>
+              <Text style={styles.myNameText}>{me.name}</Text>
+              <Text style={styles.myEmailText}>{me.email}</Text>
+            </View>
+            <TouchableOpacity onPress={onPressLogout}>
+              <Text style={styles.logoutText}>로그아웃</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Screen>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  sectionTitleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: Colors.BLACK,
+  },
+  userSectionContent: {
+    backgroundColor: Colors.BLACK,
+    borderRadius: 12,
+    padding: 20,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  myProfile: {
+    flex: 1,
+  },
+  myNameText: {
+    color: Colors.WHITE,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  myEmailText: {
+    marginTop: 4,
+    color: Colors.WHITE,
+    fontSize: 14,
+  },
+  logoutText: {
+    color: Colors.WHITE,
+    fontSize: 14,
+  },
+});
 
 export default HomeScreen;
