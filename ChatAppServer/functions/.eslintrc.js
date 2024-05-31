8cd0dd588aa6a6ce85e145899e647e9c918c628f
Yumin Chu <yuminc03@gmail.com>
2024-05-31 14:16:55 +0900

    Feat: 알림 메시지가 실시간으로 오게 하기 - backend

--- ChatAppServer/functions/.eslintrc.js
+++ ChatAppServer/functions/.eslintrc.js
@@ -16,18 +16,16 @@ module.exports = {
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
   },
 };
--- ChatAppServer/functions/src/index.ts
+++ ChatAppServer/functions/src/index.ts
@@ -1,19 +1,71 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getMessaging } from "firebase-admin/messaging";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+export const onMessageCreated = functions.firestore
+  .document("chats/{chatId}/messages/{messageId}")
+  .onCreate(async (snap, context) => {
+    // snap - 바뀐 문서의 정보
+    // context - 문서의 데이터
+    // 새 메시지가 생기면 해당 채팅방에 있는 사용자에게 push를 보낸다.
+    // 1. 어디로 메시지를 보내는지?
+    const chatId = context.params.chatId;
+    const chatSnapshot = await getFirestore()
+      .collection("chats")
+      .doc(chatId)
+      .get();
+    const chat = chatSnapshot.data();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+    if (chat == null) {
+      return;
+    }
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    const message = snap.data();
+    const senderId = message.user.userId;
+    const userIds = (chat.userIds as string[]).filter(
+      (userId) => userId != senderId
+    );
+    const usersSnapshot = await getFirestore()
+      .collection("users")
+      .where("userId", "in", userIds)
+      .get();
+    const usersFcmTokens = usersSnapshot.docs.map(
+      (doc) => doc.data().fcmTokens as string[]
+    );
+    const fcmTokens = usersFcmTokens.reduce((allTokens, tokens) => {
+      return allTokens.concat(tokens);
+    }, []);
+
+    // 2. 메시지를 보내기
+    // title - "메시지가 도착했습니다"
+    // body - "text, audio, image"
+    const messageText = (() => {
+      if (message.text != null) {
+        return message.text as string;
+      }
+
+      if (message.imageUrl != null) {
+        return "사진";
+      }
+
+      if (message.audioUrl != null) {
+        return "음성메시지";
+      }
+      return "지원하지 않는 메시지";
+    })();
+    const senderName = message.user.name;
+
+    getMessaging().sendMulticast({
+      notification: {
+        title: "메시지가 도착했습니다.",
+        body: `${senderName}: ${messageText}`,
+      },
+      data: {
+        userIds: JSON.stringify(chat.userIds),
+      },
+      tokens: fcmTokens,
+    });
+
+    console.log("Done");
+  });
