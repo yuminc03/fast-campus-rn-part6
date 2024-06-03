06dcc2d8f5ced82ebedb35981f7e054946a9a4e2
Yumin Chu <yuminc03@gmail.com>
2024-06-03 15:32:47 +0900

    Feat: Movie Detail 화면 (사진, 제목, 영어제목, 개봉일 Text) UI

---
+++ MovieReminder/src/screens/MovieScreen/useMovie.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { useCallback } from 'react';
+
+import { getMovieDetails } from '../modules/APIRequest';
+
+interface UseMovieParams {
+  id: number;
+}
+
+const useMovie = ({ id }: UseMovieParams) => {
+  const getMovie = useCallback(() => getMovieDetails({ id }), [id]);
+  const { data, isLoading } = useQuery({
+    queryKey: ['movie', id],
+    queryFn: getMovie,
+  });
+
+  return {
+    movie: data,
+    isLoading,
+  };
+};
+
+export default useMovie;
