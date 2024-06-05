f17da0925eb47681f51206e6fad7cf3892289678
Yumin Chu <yuminc03@gmail.com>
2024-06-05 15:58:50 +0900

    Feat: CodePush 업데이트 기능을 UI와 연결

--- MovieReminder/App.tsx
+++ MovieReminder/App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import CodePush from 'react-native-code-push';
 
 import { RootStackParamList } from './src/types';
 import MoviesScreen from './src/screens/MoviesScreen/MoviesScreen';
@@ -10,16 +11,13 @@ import RemindersScreen from './src/screens/RemindersScreen/RemindersScreen';
 import PurchaseScreen from './src/screens/PurchaseScreen/PurchaseScreen';
 import SubscriptionProvider from './src/components/SubscriptionProvider';
 import LoadingScreen from './src/screens/LoadingScreen/LoadingScreen';
+import useCodePush from './src/hooks/useCodePush';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const queryClient = new QueryClient();
 
 const App = () => {
-  const updating = false;
-  const progress = {
-    total: 100,
-    now: 50,
-  };
+  const { updating, progress } = useCodePush();
 
   return (
     // subscribed 정보를 어디서든 사용할 수 있게 함
@@ -32,7 +30,14 @@ const App = () => {
                 {prop => (
                   <LoadingScreen
                     {...prop}
-                    progress={progress != null ? progress : undefined}
+                    progress={
+                      progress != null
+                        ? {
+                            total: progress.totalBytes,
+                            now: progress.receivedBytes,
+                          }
+                        : undefined
+                    }
                   />
                 )}
               </Stack.Screen>
@@ -51,4 +56,5 @@ const App = () => {
   );
 };
 
-export default App;
+const codePushOptions = { checkFrequency: CodePush.CheckFrequency.MANUAL };
+export default CodePush(codePushOptions)(App);
